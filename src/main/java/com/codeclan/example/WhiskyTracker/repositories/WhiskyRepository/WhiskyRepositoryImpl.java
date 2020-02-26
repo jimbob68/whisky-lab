4a4b56f782f58b4cffe91926e55163bb85238b10
James Fraser <jimbob4t@virginmedia.com>
2020-02-26 16:31:29 +0000

    first search working

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+
+public class WhiskyRepositoryImpl implements WhiskyRepositoryCustom {
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Whisky> findWhiskyByYear(int year){
+        List<Whisky> result = null;
+        Session session = entityManager.unwrap(Session.class);
+
+        Criteria cr = session.createCriteria(Whisky.class);
+
+        cr.add(Restrictions.eq("year", year));
+        result = cr.list();
+        return result;
+    }
+}
