c6f91eec039d1ad5ad28e766de59ed769021097c
James Fraser <jimbob4t@virginmedia.com>
2020-02-26 16:53:33 +0000

    Get distilleries by region working

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -1,11 +1,26 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/distilleries")
 public class DistilleryController {
+
+    @Autowired
+    DistilleryRepository distilleryRepository;
+
+    @GetMapping(value = "/search/findAllDistilleriesInAParticularArea")
+    public List<Distillery> findAllDistilleriesInAParticularArea(@RequestParam String region){
+        return distilleryRepository.findAllDistilleriesInAParticularArea(region);
+    }
 
 
 }
--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
@@ -1,4 +1,27 @@
 package com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class DistilleryRepositoryImpl implements DistilleryRepositoryCustom {
+
+    @Autowired
+    EntityManager entitymanager;
+
+    public List<Distillery> findAllDistilleriesInAParticularArea(String region){
+      List<Distillery> result = null;
+      Session session = entitymanager.unwrap(Session.class);
+
+        Criteria cr = session.createCriteria(Distillery.class);
+
+        cr.add(Restrictions.eq("region", region));
+        result = cr.list();
+        return result;
+    }
 }
